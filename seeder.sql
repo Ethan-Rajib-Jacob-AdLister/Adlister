29ab96ea239756de67b0cdb451a017f65e1aa7be
ethan <ethan.gonzalez00774@gmail.com>
2023-01-10 09:47:37 -0800

    updated a new db

--- seeder.sql
+++ seeder.sql
@@ -1,12 +1,2 @@
 
-INSERT INTO games_for_sale_db.users (id, username, email, password) VALUES (1, 'spider_man', 'peterparker@gmail.com', '123');
-
-
-
-INSERT INTO games_for_sale_db.games (game_id, user_id, game_console, title, sale_trade, price, genre, description) VALUES (1, 1, 'playstation', 'uncharted', 'sell', '20', 'action', 'the game is a classic');
-
-INSERT INTO games_for_sale_db.games (game_id, user_id, game_console, title, sale_trade, price, genre, description) VALUES (2, 1, 'xbox', 'gears of war', 'trade', '30', 'action', 'the game is Awesome');
-
-INSERT INTO games_for_sale_db.games (game_id, user_id, game_console, title, sale_trade, price, genre, description) VALUES (3, 1, 'gamecube', 'mario cart', 'sell', '10', 'party', 'the game is old');
-
-
+-- nothing so far
